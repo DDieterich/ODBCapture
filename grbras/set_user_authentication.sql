6346bd2064945884ed2a0afdf9523be2568fabd6
Duane Dieterich <duane.dieterich+github@gmail.com>
2024-08-09 17:49:50 -0500

    Add files via upload 1

---
+++ grbras/set_user_authentication.sql
@@ -0,0 +1,11 @@
+
+--
+-- Set "grbras" USER AUTHENTICATION
+--
+-- Command Line Parameters:
+--   1 - Password Key
+--
+
+define PASSKEY="&1."
+
+
---
+++ grbsdo/installation_prepare.sql
@@ -0,0 +1,34 @@
+
+--
+--  Prepare for View Install
+--
+
+prompt
+prompt Create Temp Publicly Updateable Table
+create table TEMP_PUBLICLY_UPDATEABLE_TABLE (c1 number);
+grant all on TEMP_PUBLICLY_UPDATEABLE_TABLE to PUBLIC with grant option;
+create public synonym TEMP_PUBLICLY_UPDATEABLE_TABLE for TEMP_PUBLICLY_UPDATEABLE_TABLE;
+
+prompt
+prompt Check for Prerequisite BUILD_TYPEs
+declare
+   procedure do_it (in_btype varchar2) is
+      cursor c_main is
+         select * from ODBCAPTURE_INSTALLATION_LOGS
+          where build_type = in_btype;
+      b_main   c_main%ROWTYPE;
+   begin
+      open c_main;
+      fetch c_main into b_main;
+      if c_main%NOTFOUND
+      then
+         dbms_output.put_line('WARNING: Prerequisite BUILD_TYPE "' || in_btype ||
+                              '" not found in ODBCAPTURE_INSTALLATION_LOGS table.');
+      end if;
+      close c_main;
+   end;
+begin
+   do_it('grbsrc');
+end;
+/
+
